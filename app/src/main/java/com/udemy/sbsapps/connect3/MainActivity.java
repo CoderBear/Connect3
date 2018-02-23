e63f210ae0574b45f69a5e3e4ed9bb3134127d6b
Joe Ortega <joeortega79@gmail.com>
2018-02-23 14:00:50 +1030

    game functional with polish

--- app/src/main/java/com/udemy/sbsapps/connect3/MainActivity.java
+++ app/src/main/java/com/udemy/sbsapps/connect3/MainActivity.java
@@ -3,7 +3,11 @@ package com.udemy.sbsapps.connect3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,39 +19,73 @@ public class MainActivity extends AppCompatActivity {
                                 {0,3,6}, {1,4,7}, {2,5,8},
                                 {0,4,8}, {2,4,6}};
 
+    boolean gameActive = true;
+
     // 0 == yellow, 1 == red
     int active_player = 0;
 
     public void dropIn(View view) {
         ImageView counter = (ImageView) view;
+        TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
+        Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
 
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        String mesaage = "The ";
+        if (gameState[tappedCounter] == 2 && gameActive) {
 
-        counter.setTranslationY(-1500);
+            String mesaage = "The ";
 
-        if(active_player == 0) {
-            counter.setImageResource(R.drawable.yellow);
-            active_player = 1;
-        } else {
-            counter.setImageResource(R.drawable.red);
-            active_player = 0;
-        }
+            counter.setTranslationY(-1500);
 
-        counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+            if (active_player == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                active_player = 1;
+            } else {
+                counter.setImageResource(R.drawable.red);
+                active_player = 0;
+            }
 
-        for (int[] winningPosition : winningPositions) {
-            if(gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
-                if(active_player == 1){
-                    mesaage += "yellow player has won!";
-                } else if(active_player == 0){
-                    mesaage += "red player has won!";
+            counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+
+            for (int[] winningPosition : winningPositions) {
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
+                    if (active_player == 1) {
+                        mesaage += "yellow player has won!";
+                    } else if (active_player == 0) {
+                        mesaage += "red player has won!";
+                    }
+
+                    gameActive = false;
+
+                    winnerTextView.setText(mesaage);
+                    playAgainButton.setVisibility(View.VISIBLE);
+                    winnerTextView.setVisibility(View.VISIBLE);
+
                 }
-
-                Toast.makeText(this, mesaage, Toast.LENGTH_LONG).show();
             }
         }
+    }
+
+    public void playAgain(View view) {
+
+        TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
+        Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
+        playAgainButton.setVisibility(View.INVISIBLE);
+        winnerTextView.setVisibility(View.INVISIBLE);
+
+        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
+
+        for (int i = 0; i < gridLayout.getChildCount(); i++) {
+            ImageView counter = (ImageView) gridLayout.getChildAt(i);
+            counter.setImageDrawable(null);
+        }
+
+        for (int i = 0; i <gameState.length; i++) {
+            gameState[i] = 2;
+        }
+
+        active_player = 0;
+        gameActive = true;
     }
 
     @Override
